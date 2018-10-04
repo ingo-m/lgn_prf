307dae719580b7c48abb5267079b338cc3238fbc
Ingo Marquardt <ingo.marquardt@gmx.de>
2018-10-04 16:46:32 +0200

    Removed redundant variable.

--- analysis/sub-02/01_preprocessing/n_03b_spm_create_moco_batch_template.m
+++ analysis/sub-02/01_preprocessing/n_03b_spm_create_moco_batch_template.m
@@ -10,7 +10,6 @@ clear;
 strSubId = getenv('str_sub_id');
 % strAnalyPth = getenv('str_anly_path');
 strDataPth = getenv('str_data_path');
-varNumRuns = int16(str2num(getenv('var_num_runs')));
 % Path of the SPM moco directory:
 strPathParent = strcat(strDataPth, 'derivatives/', strSubId, '/spm_reg_within_runs/');
 % Run index (will be replaced with bash sed text replacement before this script
