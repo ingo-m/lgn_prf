13902de8d96a3aa1c0dd37f594a30c9f65a6bfbb
Ingo Marquardt <ingo.marquardt@gmx.de>
2018-10-04 16:46:13 +0200

    Bugfix SPM loop through runs (loop through run indices 01, 02, 03, etc. across runs, irrespective of session).

--- analysis/sub-02/01_preprocessing/n_03a_spm_moco_parallel.sh
+++ analysis/sub-02/01_preprocessing/n_03a_spm_moco_parallel.sh
@@ -52,7 +52,7 @@ do
     strTmpSpmCnt=`printf %02d ${var_cnt_run}`
 
     # File name for new SPM file:
-    strTmpSpm="${str_anly_path}${str_sub_id}/01_preprocessing/spm_moco_batches/${str_sub_id}_${idx_ses_id}_run_${strTmpSpmCnt}.m"
+    strTmpSpm="${str_anly_path}${str_sub_id}/01_preprocessing/spm_moco_batches/run_${strTmpSpmCnt}.m"
 
     # Copy template SPM file:
     cp ${strTmplt} ${strTmpSpm}
@@ -81,43 +81,36 @@ date
 # Session counter:
 var_cnt_ses=0
 
-# Loop through sessions (e.g. "ses-01"):
-for idx_ses_id in ${ary_ses_id[@]}
+# Subtract one from run counter (because bash sequence includes endpoint):
+var_cnt_run=`bc <<< ${var_cnt_run}-1`
+
+# Loop through runs (e.g. "run_01"); i.e. zero filled indices ("01", "02",
+# etc.).
+for idx_num_run in $(seq -f "%02g" 1 ${var_cnt_run})
 do
 
-  # Loop through runs (e.g. "run_01"); i.e. zero filled indices ("01", "02",
-  # etc.). Note that the number of runs may not be identical throughout
-  # sessions.
-  for idx_num_run in $(seq -f "%02g" 1 ${ary_num_runs[var_cnt_ses]})
-  do
+  # Run SPM moco:
+  /opt/spm12/run_spm12.sh /opt/mcr/v85/ batch ${str_anly_path}${str_sub_id}/01_preprocessing/spm_moco_batches/run_${idx_num_run}.m &
 
-    # Run SPM moco:
-    /opt/spm12/run_spm12.sh /opt/mcr/v85/ batch ${str_anly_path}${str_sub_id}/01_preprocessing/spm_moco_batches/${str_sub_id}_${idx_ses_id}_run_${idx_num_run}.m &
+  # Wait for SPM startup:
+  sleep 20
 
-    # Wait for SPM startup:
-    sleep 20
-
-    # Check whether it's time to issue a wait command (if the modulus of the
-    # index and the parallelisation-value is zero). The purpose of the prefix
-    # "10#" is to interpret the zero-filled run number (e.g. "08") as a decimal
-    # number, and not as an octal number.
-    if [[ $((10#${idx_num_run} + 1))%${varPar} -eq 0 ]]
+  # Check whether it's time to issue a wait command (if the modulus of the
+  # index and the parallelisation-value is zero). The purpose of the prefix
+  # "10#" is to interpret the zero-filled run number (e.g. "08") as a decimal
+  # number, and not as an octal number.
+  if [[ $((10#${idx_num_run} + 1))%${varPar} -eq 0 ]]
+  then
+    # Only issue a wait command if the index is greater than zero (i.e.,
+    # not for the first segment):
+    if [[ 10#${idx_num_run} -gt 0 ]]
     then
-      # Only issue a wait command if the index is greater than zero (i.e.,
-      # not for the first segment):
-      if [[ 10#${idx_num_run} -gt 0 ]]
-      then
-        wait
-        echo "------Progress: $((10#${idx_num_run})) runs out of" \
-        "${ary_num_runs[var_cnt_ses]}"
-      fi
+      wait
+      echo "------Progress: $((10#${idx_num_run})) runs out of ${var_cnt_run}"
     fi
-  done
-  wait
-
-  # Increment session counter:
-  var_cnt_ses=`bc <<< ${var_cnt_ses}+1`
-
+  fi
 done
+wait
+
 date
 #------------------------------------------------------------------------------
